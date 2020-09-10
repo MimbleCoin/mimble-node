44501f4d8bb2f94a27039be6eec92e2b045af928
Forkladdgud <70984818+Forkladdgud@users.noreply.github.com>
2020-09-10 16:46:45 +0200

    Update ui.rs

--- src/bin/tui/ui.rs
+++ src/bin/tui/ui.rs
@@ -94,7 +94,7 @@ impl UI {
 		let mut title_string = StyledString::new();
 		title_string.append(StyledString::styled(
 			format!(
-				"MWC Version {} (proto: {})",
+				"Mimble Node Version {} (proto: {})",
 				built_info::PKG_VERSION,
 				Server::protocol_version()
 			),
